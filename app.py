08e3310a3e9e8f0a63b5bb303ff74c1b6ff0c676
AI Cortex <artificial.intelligence.cortex@gmail.com>
2025-11-23 04:36:06 +0330

    add project

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from src.ui.layout import build_ui
 import os
+import argparse
 
 
 def main():
     os.environ["NO_PROXY"] = "127.0.0.1,localhost"
     os.environ["http_proxy"] = ""
     os.environ["https_proxy"] = ""
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--share")
+    args = parser.parse_args()
+    
     demo = build_ui()
-    demo.launch(share=True)
+    demo.launch(share=args.share)
 
 
 if __name__ == "__main__":
